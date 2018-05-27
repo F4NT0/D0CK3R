cfb558d9a218edd66c97ddf1b69a85df2e841258
Gabriel Fanto Stundner <gabriel.stundner@acad.pucrs.br>
2018-05-27 11:54:59 -0300

    Script Hello-World

---
+++ DockerImages.sh
@@ -0,0 +1,6 @@
+#SCRIPT PARA RODAR DOCKER NO SISTEMA
+# PARA FAZER EXECUTAVEL ESSE PROGRAMA: chmod +x DockerImages.sh
+# PARA INICIAR O PROGRAMA: ./DockerImages.sh
+
+printf "Iniciating a test with Docker"
+${sudo docker run -ti hello-world}
